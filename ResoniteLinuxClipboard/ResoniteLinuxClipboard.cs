89ad17c7654a806ad2159e432da7fc656fceff43
Grand <grand.uk@tutanota.com>
2025-08-20 13:56:11 +0100

    clearning up old pre-splittening methods

--- ResoniteLinuxClipboard/ResoniteLinuxClipboard.cs
+++ ResoniteLinuxClipboard/ResoniteLinuxClipboard.cs
@@ -9,12 +9,12 @@ using ResoniteModLoader;
 using Elements.Assets;
 using Renderite.Shared;
 using System.Threading.Tasks;
-using FrooxEngine.UIX;
 using Elements.Core;
+using System.Runtime.CompilerServices;
 
 namespace ResoniteLinuxClipboard;
 
-public partial class ResoniteLinuxClipboard : ResoniteMod {
+public class ResoniteLinuxClipboard : ResoniteMod {
 	internal const string VERSION_CONSTANT = "1.0.0"; 
 	public override string Name => "ResoniteLinuxClipboard";
 	public override string Author => "Grand";
@@ -22,77 +22,63 @@ public partial class ResoniteLinuxClipboard : ResoniteMod {
 	public override string Link => "https://github.com/resonite-modding-group/ExampleMod/";
 
 	public override void OnEngineInit() {
-		Harmony harmony = new Harmony("com.example.ExampleMod");
-		harmony.PatchAll();
 		Engine.Current.RunPostInit( () => {
 			Engine.Current.InputInterface.RegisterClipboardInterface(new ResoniteLinuxClipboardInterface());
+			Msg("clipboard registered");
 		});
 		// Console.WriteLine("mod loaded but not active");
 
 	}
-	public partial class ResoniteLinuxClipboardInterface : IClipboardInterface {
-		public bool ContainsText => false;
+}
+public partial class ResoniteLinuxClipboardInterface : IClipboardInterface {
+	public bool ContainsText => false;
 
-		public bool ContainsFiles => false;
+	public bool ContainsFiles => false;
 
-		public bool ContainsImage => false;
+	public bool ContainsImage => false;
 
-		public void Dispose() {
-			Console.WriteLine("disposing");
-		}
-
-		public List<string> GetFiles() {
-			return [""];
-		}
-
-		public Bitmap2D GetImage() {
-			Bitmap2D bitmap = new([], 0, 0, TextureFormat.BC7, false, ColorProfile.sRGBAlpha);
-			return bitmap;
-		}
-
-		public string GetText() {
-			return "";
-		}
-
-		public void SetBitmap(Bitmap2D bitmap) {
-			int count = bitmap.ElementTotalBytes;
-			byte[] bytes = bitmap.RawData.ToArray();
-			copy(bytes,(uint)count);
-			Console.WriteLine("Attempted copy image");
-		}
-
-		public void SetText(string text) {
-			byte[] bytes = Encoding.UTF8.GetBytes(text);
-			copy(bytes, (uint)bytes.Length);
-			Console.WriteLine("Attempted copy text");
-		}
-
-		async Task<string> IClipboardInterface.GetText() {
-			string result = GetText();
-			await Task.Delay(10);
-			return result;
-		}
-
-		Task<List<string>> IClipboardInterface.GetFiles() {
-			return Task.FromResult(GetFiles());
-		}
-
-		Task<Bitmap2D> IClipboardInterface.GetImage() {
-			return Task.FromResult(GetImage());
-		}
-
-		Task<bool> IClipboardInterface.SetText(string text) {
-			SetText(text);
-			return Task.FromResult(true);
-		}
-
-		Task<bool> IClipboardInterface.SetBitmap(Bitmap2D bitmap) {
-			SetBitmap(bitmap);
-			return Task.FromResult(true);
-		}
-
-
-		[LibraryImport("libresoniteclipboard_rs.so")]
-		private static partial void copy(byte[] data, uint data_length);
+	public void Dispose() {
+		UniLog.Log("disposing");
+		UniLog.Flush();
 	}
+
+	async Task<string> IClipboardInterface.GetText() {
+		await Task.Run(() => {
+			return "";
+		});
+		return "";
+	}
+
+	Task<List<string>> IClipboardInterface.GetFiles() {
+		UniLog.Log("attempting get files");
+		UniLog.Flush();
+		return Task.FromResult<List<string>>([]);
+	}
+
+	Task<Bitmap2D> IClipboardInterface.GetImage() {
+		UniLog.Log("attempting get image");
+		UniLog.Flush();
+		return Task.FromResult<Bitmap2D>(null);
+	}
+
+	Task<bool> IClipboardInterface.SetText(string text) {
+		byte[] bytes = Encoding.UTF8.GetBytes(text);
+		Copy(bytes, (uint)bytes.Length);
+		UniLog.Log("Attempted copy text");
+		UniLog.Flush();
+		return Task.FromResult(true);
+	}
+
+	Task<bool> IClipboardInterface.SetBitmap(Bitmap2D bitmap) {
+		int count = bitmap.ElementTotalBytes;
+		byte[] bytes = bitmap.RawData.ToArray();
+		Copy(bytes,(uint)count);
+		UniLog.Log("Attempted copy image");
+		UniLog.Flush();
+		return Task.FromResult(true);
+	}
+
+
+	[LibraryImport("libresoniteclipboard_rs", EntryPoint = "copy")]
+	private static partial void Copy(byte[] data, uint data_length);
 }
--- resoniteclipboard-rs/src/lib.rs
+++ resoniteclipboard-rs/src/lib.rs
@@ -7,8 +7,11 @@ use wl_clipboard_rs::copy::{MimeType, Options};
 
 
 #[unsafe(no_mangle)]
-pub unsafe extern fn copy(data: *const c_uchar, data_length: u32) {
+pub extern "C" fn copy(data: *const c_uchar, data_length: u32) {
     let a = unsafe { slice::from_raw_parts(data,data_length.try_into().unwrap()) };
     let opts = Options::new();
-    let _ = opts.copy(wl_clipboard_rs::copy::Source::Bytes(a.into()), MimeType::Autodetect);
+    match opts.copy(wl_clipboard_rs::copy::Source::Bytes(a.into()), MimeType::Autodetect) {
+        Ok(_) => println!("copy success"),
+        Err(_) => todo!("copy failure"),
+    }
 }
